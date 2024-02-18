9e52a8aea1cacc44f9b06166018541ed02c348a5
RonexLemon <ronexondimu@gmail.com>
2024-02-18 05:46:06 +0300

    connect to db

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,23 @@ const express = require("express");
 //init app middleware
 const cors = require("cors");
 
+const {connectToDb,getDb} = require("../Backend/config/db")
 const books = require("../Backend/routes/Books/route");
 
 const app = express(); // Initialize Express app
+//database connect
+let db;
+connectToDb((err)=>{
+    if (!err){
+        app.listen(3000, () => {
+            console.log(`App listening on port ${3000}`);
+        });
+        db = getDb()
+        
+
+    }
+
+})
 
 app.use(cors()); // Use cors middleware
 
@@ -12,6 +26,3 @@ app.use(cors()); // Use cors middleware
 app.use("/books", books);
 
 
-app.listen(3000, () => {
-    console.log(`App listening on port ${3000}`);
-});
