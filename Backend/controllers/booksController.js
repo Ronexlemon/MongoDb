cb5a5956b6d04dc08a4d60ccd8e98fefed339a7d
RonexLemon <ronexondimu@gmail.com>
2024-02-19 05:04:32 +0300

    same genres

--- Backend/controllers/booksController.js
+++ Backend/controllers/booksController.js
@@ -25,7 +25,7 @@ const AllBooks =asyncHandler(async(req,res)=>{
 
 const getBookByAuthor = asyncHandler(async(req,res)=>{
     const { name,id } = req.body;
-    console.log("name name name",name,id)
+   
 
     const db = getDb()
     let books =[]
@@ -41,8 +41,26 @@ const getBookByAuthor = asyncHandler(async(req,res)=>{
 
 
 })
+const getBooksFromSameGenres = asyncHandler(async(req,res)=>{
+    const { genre } = req.body;
+   
+
+    const db = getDb()
+    let books =[]
+    const data =  db.collection("books2").find({genres: genre})
+        .forEach(book => books.push(book))
+    .then(()=>{
+      res.status(200).json(books)
+    })
+    .catch(()=>{
+      res.status(500).json({error: "Could not fetch the documents"})
+    });
+
+
+})
 
 module.exports ={
     AllBooks,
-    getBookByAuthor
+    getBookByAuthor,
+    getBooksFromSameGenres
 }
--- Backend/routes/Books/route.js
+++ Backend/routes/Books/route.js
@@ -1,12 +1,13 @@
 const express = require("express")
 
 const router = express.Router()
-const {AllBooks, getBookByAuthor} = require("../../controllers/booksController")
+const {AllBooks, getBookByAuthor, getBooksFromSameGenres} = require("../../controllers/booksController")
 
 
 
 router.get("/all",AllBooks)
 router.post("/author",getBookByAuthor)
+router.get("/genres",getBooksFromSameGenres)
 
 module.exports =  router;
 
